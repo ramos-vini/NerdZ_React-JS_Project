22e1ce9e91d3608296031c312034e677d1582b05
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-21 19:24:01 -0300

    created 'fixedCart' component

---
+++ src/components/fixedCart/FixedCart.jsx
@@ -0,0 +1,10 @@
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import './fixedCart.css';
+
+export default function FixedCart(params) {
+    return (
+        <>
+            <ShoppingCartIcon className='cart'></ShoppingCartIcon>
+        </>
+    );
+}

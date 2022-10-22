41309143cbc920314bbc3ab9be6b90ae21f75789
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-22 19:15:50 -0300

    Modelo de Rotas adicionado ao App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Catalogo from './pages/catalogo/Catalogo';
 import Login from '../src/pages/login/Login'
 import Registro from './pages/registro/Registro';
 
-// Nerd + (alguma coisa) - Plataforma de venda de cursos nerds (dev, games, series, etc.);
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
-      <Registro/>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+        <Route exact path="/" element={<Catalogo />} />
+        <Route exact path="/home" element={<Catalogo />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/registro" element={<Registro />} />
+          {/*
+          <Route exact path="/produto" element={<Produto />} />
+          <Route path="*" element={<NotFound />} />
+          */}
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 

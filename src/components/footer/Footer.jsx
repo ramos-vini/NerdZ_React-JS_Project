17ad0968da4b5dc2202cfe406b34b17a44c967d7
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-22 17:57:36 -0300

    Estilização e responsividade do Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,26 @@
+import './footer.css';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+
+export default function Footer(params) {
+    return (
+        <nav className='footerNav'>
+            <div className='footerNavDiv1'>
+                <section className='footerNavDiv1Sec1'>
+                    <h4>Nosso Objetivo</h4>
+                    <p>A NerdZ foi criada para levar ao público da cultura geek conhecimentos aprofundados sobre seus assuntos favoritos, sejam videogames, séries, filmes, HQs ou ramos da tecnologia. Tudo que um Nerd precisa, ele encontra aqui!</p>
+                    <p><EmailOutlinedIcon fontSize='small'></EmailOutlinedIcon> <i>atendimento@nerdz.com.br</i></p>
+                </section>
+
+                <section className='footerNavDiv1Sec2'>
+                    <h4>Baixe nosso App</h4>
+                    <a href><img src={require('../../assets/img/app-store.png')} alt="App disponível na App Store" style={{ width: '170px' }} /></a>
+                    <a href><img src={require('../../assets/img/google-play.png')} alt="App disponível na Google Play" style={{ width: '170px' }} /></a>
+                </section>
+            </div>
+
+            <div className='footerNavDiv2'>
+                <h2 className='logo logoFooter'><a href>NerdZ</a></h2>
+            </div>
+        </nav>
+    );
+}

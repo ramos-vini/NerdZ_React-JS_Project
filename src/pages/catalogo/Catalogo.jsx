f5678b6b281ce2eb91f8a5ccdc3bfb6b2f396178
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-22 19:16:38 -0300

    Criação da page Catalogo

---
+++ src/pages/catalogo/Catalogo.jsx
@@ -0,0 +1,16 @@
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+import './catalogo.css';
+
+export default function Catalogo(params) {
+    return (
+        <>
+            <Header></Header>
+
+            <h1>Catálogo</h1>
+
+            <Footer></Footer>
+        </>
+    );
+}

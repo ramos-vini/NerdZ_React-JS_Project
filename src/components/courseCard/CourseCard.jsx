ec1da4d3e578fa2df357521d7f57b40c9de9c0d0
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-24 10:58:25 -0300

    Criação do Component CourseCard

---
+++ src/components/courseCard/CourseCard.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Rating } from '@mui/material';
+import './courseCard.css';
+
+export default function CourseCard(props) {
+  return (
+    <Card sx={{ maxWidth: 345}} className='courseCard'>
+      <CardMedia
+        component="img"
+        height="140"
+        image= {props.img}
+        alt="course img"
+        className='courseCardMedia'
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.text}
+        </Typography>
+        <Rating name="read-only" value={5} readOnly sx={{marginTop: '4%'}}/>
+      </CardContent>
+      <CardActions>
+        <Button size="small" sx={{color: '#FF731D'}}>Conheça o Curso</Button>
+      </CardActions>
+    </Card>
+  );
+}

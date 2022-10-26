edc5099b4065faf7846d34112b8162c4da7f3801
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-26 17:40:59 -0300

    Estilização e responsividade do CourseCard.jsx

--- src/components/courseCard/CourseCard.jsx
+++ src/components/courseCard/CourseCard.jsx
@@ -6,30 +6,40 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Rating } from '@mui/material';
+import { Grid } from "@mui/material";
 import './courseCard.css';
 
 export default function CourseCard(props) {
   return (
-    <Card sx={{ maxWidth: 345}} className='courseCard'>
-      <CardMedia
-        component="img"
-        height="140"
-        image= {props.img}
-        alt="course img"
-        className='courseCardMedia'
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {props.nome}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {props.descricao}
-        </Typography>
-        <Rating name="read-only" value={5} readOnly sx={{marginTop: '4%'}}/>
-      </CardContent>
-      <CardActions>
-        <Button size="small" sx={{color: '#FF731D'}}>Conheça o Curso</Button>
-      </CardActions>
-    </Card>
+    <Grid item xs={12} sm={6} md={4}>
+
+      <Card sx={{ maxWidth: 345, height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}} className='courseCard'>
+        <div>
+        <CardMedia
+          component="img"
+          height="140"
+          image={props.img}
+          alt="course img"
+          className='courseCardMedia'
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.nome}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.descricao}
+          </Typography>
+          <Rating name="read-only" value={5} readOnly sx={{ marginTop: '4%' }} />
+        </CardContent>
+        </div>
+
+        <div>
+        <CardActions>
+          <Button size="small" sx={{ color: '#FF731D' }}>Conheça o Curso</Button>
+        </CardActions>
+        </div>
+      </Card>
+
+    </Grid>
   );
 }

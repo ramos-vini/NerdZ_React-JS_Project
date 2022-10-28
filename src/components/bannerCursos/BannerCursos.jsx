dc21bb6d6b0384b1134aa7387401bcaa7244f9e2
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-28 13:32:26 -0300

    BannerCursos criado e adc ao Catalogo.jsx

---
+++ src/components/bannerCursos/BannerCursos.jsx
@@ -0,0 +1,10 @@
+import './bannerCursos.css';
+
+export default function BannerCursos(params) {
+    return (
+        <div className='bannerCursos'>
+            <h2>Os melhores cursos</h2>
+            <h5>Tudo que um Nerd precisa ele encontra aqui!</h5>
+        </div>
+    );
+}

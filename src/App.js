3f18b76e20c873c940036b0e83550f49505c6b0c
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-28 16:30:34 -0300

    Page NotFound.jsx criada

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Catalogo from './pages/catalogo/Catalogo';
 import Login from '../src/pages/login/Login'
 import Registro from './pages/registro/Registro';
+import Produto from './pages/produto/Produto';
+import NotFound from './pages/notFound/NotFound';
 
 import {
   BrowserRouter,
@@ -15,14 +17,13 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-        <Route exact path="/" element={<Catalogo />} />
-        <Route exact path="/home" element={<Catalogo />} />
+          <Route exact path="/" element={<Catalogo />} />
+          <Route exact path="/home" element={<Catalogo />} />
+          <Route exact path="/catalogo" element={<Catalogo />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/registro" element={<Registro />} />
-          {/*
           <Route exact path="/produto" element={<Produto />} />
           <Route path="*" element={<NotFound />} />
-          */}
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/notFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import './notfound.css';
+import { ReactComponent as NotFoundSVG } from '../../assets/img/404.svg';
+import { Link } from "react-router-dom";
+
+export default function NotFound(params) {
+    return(
+        <div className='divNotFound'>
+            <NotFoundSVG className='notFoundSVG'></NotFoundSVG>
+            <h1>Ops... Página não encontrada.</h1>
+            <h5 style={{marginTop: '4%'}}><Link to="/" className="mainBannerLink">Página Inicial</Link></h5>
+        </div>
+    )
+}

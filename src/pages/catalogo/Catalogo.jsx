b9dd4914181b656696725e6e050106bb68a537c5
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-28 13:35:03 -0300

    Inclusão de Components ao Catalogo.jsx

--- src/pages/catalogo/Catalogo.jsx
+++ src/pages/catalogo/Catalogo.jsx
@@ -6,10 +6,10 @@ import Vantagens from "../../components/vantagens/Vantagens";
 import SportsEsportsOutlinedIcon from '@mui/icons-material/SportsEsportsOutlined';
 import MovieOutlinedIcon from '@mui/icons-material/MovieOutlined';
 import CodeOutlinedIcon from '@mui/icons-material/CodeOutlined';
+import BannerCursos from "../../components/bannerCursos/BannerCursos";
+import Instrutor from "../../components/instrutor/Instrutor";
 import './catalogo.css';
 
-// TODO: Adc Categoria.jsx (3x): Videogames, Dev e Séries;
-// TODO: Adc Component para produtores de conteúdo ao final da página;
 // TODO: Adc JS para titulo 'Geek' do banner principal alternar com outras palavras em modelo de digitação.
 
 export default function Catalogo(params) {
@@ -21,9 +21,13 @@ export default function Catalogo(params) {
 
             <Vantagens></Vantagens>
 
+            <BannerCursos></BannerCursos>
+
             <ListaCategoria categoria="videogames" icon={<SportsEsportsOutlinedIcon className='listaCategoriaH2Icon'/>} title="Videogames"></ListaCategoria>
             <ListaCategoria categoria="series" icon={<MovieOutlinedIcon className='listaCategoriaH2Icon'/>} title="Séries & Filmes"></ListaCategoria>
             <ListaCategoria categoria="dev" icon={<CodeOutlinedIcon className='listaCategoriaH2Icon'/>} title="Desenvolvimento"></ListaCategoria>
+
+            <Instrutor></Instrutor>
 
             <Footer></Footer>
         </>

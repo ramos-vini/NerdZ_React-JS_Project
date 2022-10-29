212c1f98a45eda2b65c32d0ac0619b1be75fc446
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-29 11:14:54 -0300

    Component ValorProduto criado

---
+++ src/components/valorProduto/ValorProduto.jsx
@@ -0,0 +1,20 @@
+import './valorProduto.css';
+import Cursos from '../../global/cursos';
+import { Link } from "react-router-dom";
+
+export default function ValorProduto(params) {
+
+    let curso = Cursos[0];
+
+    return (
+        <div className='valorProduto'>
+
+            <h1>{curso.nome}</h1>
+            <h5>{curso.descricao}</h5>
+            <h3>DE <span className='oldPrice'>R${((curso.valor)*1.4).toFixed(2).replace(".",',')}</span> POR APENAS 12X DE R${((curso.valor)/12).toFixed(2).replace(".",',')}</h3>
+            <h3>PROMOÇÃO POR TEMPO LIMITADO!</h3>
+            <h4 style={{marginTop: '4%'}}><Link to="/registro" className="CTA CTAMobile">QUERO APROVEITAR ESSA OPORTUNIDADE!</Link></h4>
+
+        </div>
+    );
+}

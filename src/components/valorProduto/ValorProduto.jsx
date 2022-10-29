a6b166c521d980ae3e68270e6f94a12828c9cc3e
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-29 16:30:35 -0300

    padding ValorProduto ajustado

--- src/components/valorProduto/ValorProduto.jsx
+++ src/components/valorProduto/ValorProduto.jsx
@@ -13,7 +13,7 @@ export default function ValorProduto(params) {
             <h5>{curso.descricao}</h5>
             <h3>DE <span className='oldPrice'>R${((curso.valor)*1.4).toFixed(2).replace(".",',')}</span> POR APENAS 12X DE R${((curso.valor)/12).toFixed(2).replace(".",',')}</h3>
             <h3>PROMOÇÃO POR TEMPO LIMITADO!</h3>
-            <h4 style={{marginTop: '4%'}}><Link to="/registro" className="CTA CTAMobile">QUERO APROVEITAR ESSA OPORTUNIDADE!</Link></h4>
+            <h4 style={{margin: '4% 0'}}><Link to="/registro" className="CTA CTAMobile">QUERO APROVEITAR ESSA OPORTUNIDADE!</Link></h4>
 
         </div>
     );

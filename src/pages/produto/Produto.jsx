4db0297f7ef367b7c2c1e169cbe86b6f0beafa35
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-29 16:35:54 -0300

    Feedback e Faq adcs ao Produto.jsx

--- src/pages/produto/Produto.jsx
+++ src/pages/produto/Produto.jsx
@@ -4,11 +4,10 @@ import Footer from '../../components/footer/Footer';
 import VideoBanner from '../../components/videoBanner/VideoBanner';
 import ValorProduto from '../../components/valorProduto/ValorProduto';
 import Vantagens2 from '../../components/vantagens2/Vantagens2';
+import Feedback from '../../components/feedback/Feedback';
+import Faq from '../../components/faq/Faq';
 
-// TODO: div Feedback alunos (carrossel)
-// TODO: Call to action nº2
-// TODO: div FAQ 
-// TODO: Garantia de 7 dias 
+// TODO: Garantia de 7 dias
 // TODO: Call to action nº3
 
 export default function Produto(params) {
@@ -22,6 +21,10 @@ export default function Produto(params) {
             <ValorProduto></ValorProduto>
 
             <Vantagens2></Vantagens2>
+
+            <Feedback></Feedback>
+
+            <Faq></Faq>
 
             <Footer></Footer>
         </>

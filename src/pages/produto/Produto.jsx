d789d4d25a644ce0636b9d3d454424b10715aa6c
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-29 11:17:18 -0300

    Renderização de Components na page Produto

--- src/pages/produto/Produto.jsx
+++ src/pages/produto/Produto.jsx
@@ -1,37 +1,27 @@
 import './produto.css';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
-import React from "react";
-import Cursos from '../../global/cursos';
-// import { useState } from 'react';
+import VideoBanner from '../../components/videoBanner/VideoBanner';
+import ValorProduto from '../../components/valorProduto/ValorProduto';
+import Vantagens2 from '../../components/vantagens2/Vantagens2';
+
+// TODO: div Feedback alunos (carrossel)
+// TODO: Call to action nº2
+// TODO: div FAQ 
+// TODO: Garantia de 7 dias 
+// TODO: Call to action nº3
 
 export default function Produto(params) {
-
-    // const [curso, setCurso] = useState(cursoControl);
-
-    let curso = Cursos[0];
 
     return (
         <>
             <Header></Header>
 
-            <>
+            <VideoBanner></VideoBanner>
 
-                <div className='bannerVideo' style={{ backgroundImage: `url(${curso.img})` }}>
-                    <div className="video-responsive">
-                        <iframe className='iframe' width="560" height="315" src={curso.video} title="YouTube video player" modestbranding rel="0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
-                </div>
+            <ValorProduto></ValorProduto>
 
-                {/* TODO: div com Titulo, Descrição e Valor de X por apenas 12x de Y - Promo por Tempo Limitado */}
-                {/* TODO: Call to action nº1*/}
-                {/* TODO: div Vantagens(2) com descrição - Acesso Vitalício, Suporte Premium, Certificado Profissional, Garantia de 7 dias */}
-                {/* TODO: div Feedback alunos (carrossel)*/}
-                {/* TODO: Call to action nº2*/}
-                {/* TODO: div FAQ */}
-                {/* TODO: Garantia de 7 dias */}
-                {/* TODO: Call to action nº3*/}
-            </>
+            <Vantagens2></Vantagens2>
 
             <Footer></Footer>
         </>

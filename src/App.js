d11eb66e28a2365dfb2754c2cc78f3c48131c4dd
Vinicius Ramos <ramoscontrole@gmail.com>
2022-11-01 18:04:39 -0300

    Rotas dinâmicas de produtos individuais criada

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import Catalogo from './pages/catalogo/Catalogo';
 import Login from '../src/pages/login/Login'
 import Registro from './pages/registro/Registro';
 import Produto from './pages/produto/Produto';
+import Checkout from './pages/checkout/Checkout';
 import NotFound from './pages/notFound/NotFound';
+
+import Cursos from './global/cursos';
 
 import {
   BrowserRouter,
   Routes,
   Route
 } from 'react-router-dom';
+
 
 function App() {
   return (
@@ -22,7 +26,16 @@ function App() {
           <Route exact path="/catalogo" element={<Catalogo />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/registro" element={<Registro />} />
-          <Route exact path="/produto" element={<Produto />} />
+          {/* Rotas de vizualização individual dos produtos: */}
+          {
+            Cursos.map((curso, idx) => {
+              return (
+                <Route exact path={`/produto/${(curso.nome).replace(/ /g, "-").toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")}`} element={<Produto idx={idx} />} />
+              )
+            })
+          }
+          {/* Rotas de checkout dos produtos: */}
+          <Route exact path="/checkout" element={<Checkout />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>

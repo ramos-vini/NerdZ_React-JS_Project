adae1f542ced293522eb66f04a3a3c67d8ed1976
Vinícius Ramos <ramoscontrole@gmail.com>
2022-11-02 20:09:33 -0300

    scroll behavior initial para Link de NotFound.jsx

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -7,7 +7,7 @@ export default function NotFound(params) {
         <div className='divNotFound'>
             <NotFoundSVG className='notFoundSVG'></NotFoundSVG>
             <h1>Ops... Página não encontrada.</h1>
-            <h5 style={{marginTop: '4%'}}><Link to="/" className="mainBannerLink">Página Inicial</Link></h5>
+            <h5 style={{marginTop: '4%'}}><Link to="/" className="mainBannerLink" onClick={() => {window.scrollTo({top: 0, left: 0, behavior: 'initial'});}}>Página Inicial</Link></h5>
         </div>
     )
 }

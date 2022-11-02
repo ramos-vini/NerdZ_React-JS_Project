a8ac93f2606e77a3b5520aed0951b0994fd53254
Vinícius Ramos <ramoscontrole@gmail.com>
2022-11-02 20:04:48 -0300

    Visibilidade de Senha adc no Login e Registro

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,13 +1,42 @@
 import './login.css';
 import { TextField, FormControl, Button, Stack } from '@mui/material';
-import {theme, OrangeTextField} from '../../global/muiTheme';
+import { theme, OrangeTextField } from '../../global/muiTheme';
 import LoginIcon from '@mui/icons-material/Login';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 export default function Login(params) {
 
-    // TODO: Habilitar alternância de visibilidade da Senha, no Login e no Registro
+    const [values, setValues] = React.useState({
+        amount: '',
+        password: '',
+        weight: '',
+        weightRange: '',
+        showPassword: false,
+    });
+
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+    };
+
+    const handleClickShowPassword = () => {
+        setValues({
+            ...values,
+            showPassword: !values.showPassword,
+        });
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
 
     return (
         <>
@@ -15,24 +44,45 @@ export default function Login(params) {
 
             <Stack className='mainDiv'>
 
-            <FormControl sx={{padding: '5%'}} className='form'>
+                <FormControl sx={{ padding: '5%' }} className='form'>
 
-            <LoginIcon className='loginIcon' fontSize='large'></LoginIcon>    
+                    <LoginIcon className='loginIcon' fontSize='large'></LoginIcon>
 
-            <h1>Faça seu Login</h1>
+                    <h1>Faça seu Login</h1>
 
-            <TextField className='input' label="E-mail" variant="outlined" type="email" sx={OrangeTextField}/>
+                    <TextField label="E-mail" variant="outlined" type="email" sx={OrangeTextField} />
 
-            <TextField className='input' id="outlined-basic1" label="Senha" variant="outlined" type="password" sx={OrangeTextField}/> 
+                    <FormControl variant="outlined" sx={OrangeTextField}>
+                        <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-password"
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange('password')}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="Senha"
+                        />
+                    </FormControl>
 
-            <Button variant="contained" theme={theme} sx={{color: 'white'}}>Entrar</Button>
+                    <Button variant="contained" theme={theme} sx={{ color: 'white' }}>Entrar</Button>
 
-            <p><a href>Esqueci minha senha</a></p>
+                    <p><a href>Esqueci minha senha</a></p>
 
-            </FormControl>
+                </FormControl>
 
             </Stack>
-            
+
             <Footer></Footer>
         </>
     );
--- src/pages/registro/Registro.jsx
+++ src/pages/registro/Registro.jsx
@@ -1,39 +1,123 @@
 import './registro.css';
 import { TextField, FormControl, Button, Stack } from '@mui/material';
-import {theme, OrangeTextField} from '../../global/muiTheme';
+import { theme, OrangeTextField } from '../../global/muiTheme';
 import VideoLibraryOutlinedIcon from '@mui/icons-material/VideoLibraryOutlined';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 export default function Registro(params) {
 
+    const [values, setValues] = React.useState({
+        amount: '',
+        password: '',
+        weight: '',
+        weightRange: '',
+        showPassword: false,
+    });
+
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+    };
+
+    const handleClickShowPassword = () => {
+        setValues({
+            ...values,
+            showPassword: !values.showPassword,
+        });
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const [values2, setValues2] = React.useState({
+        amount: '',
+        password: '',
+        weight: '',
+        weightRange: '',
+        showPassword: false,
+    });
+
+    const handleChange2 = (prop) => (event) => {
+        setValues2({ ...values2, [prop]: event.target.value });
+    };
+
     return (
         <>
-
-            <Header/>
+            <Header />
 
             <Stack className='mainDiv'>
-            <FormControl sx={{padding: '5%'}} className='form'>
-            <VideoLibraryOutlinedIcon className='videoIcon' fontSize='large'></VideoLibraryOutlinedIcon>    
+                <FormControl sx={{ padding: '5%' }} className='form'>
+                    <VideoLibraryOutlinedIcon className='videoIcon' fontSize='large'></VideoLibraryOutlinedIcon>
 
-            <h1>Faça sua matrícula grátis</h1>
-        
-            <TextField className='input' label="Nome Completo" variant="outlined" type="text" sx={OrangeTextField}/>
+                    <h1>Faça sua matrícula grátis</h1>
 
-            <TextField className='input' label="E-mail" variant="outlined" type="email" sx={OrangeTextField}/>
+                    <TextField label="Nome Completo" variant="outlined" type="text" sx={OrangeTextField} />
 
-            <TextField className='input' id="outlined-basic1" label="Senha" variant="outlined" type="password" sx={OrangeTextField}/> 
+                    <TextField label="E-mail" variant="outlined" type="email" sx={OrangeTextField} />
 
-            <TextField className='input' id="outlined-basic2" label="Confirme sua senha" variant="outlined" type="password" sx={OrangeTextField}/>
+                    <FormControl variant="outlined" sx={OrangeTextField}>
+                        <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-password"
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange('password')}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="Senha"
+                        />
+                    </FormControl>
 
-            <Button variant="contained" theme={theme} sx={{color: 'white'}}>Cadastrar</Button>
+                    <FormControl variant="outlined" sx={OrangeTextField}>
+                        <InputLabel htmlFor="outlined-adornment-password-2">Confirme sua Senha</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-password-2"
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values2.password}
+                            onChange={handleChange2('password')}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="Confirme sua Senha"
+                        />
+                    </FormControl>
 
-            <p>Ao clicar em cadastrar, você declara que leu e concorda com nossos <a href>termos de utilização</a>.</p>
-            </FormControl>
+                    <Button variant="contained" theme={theme} sx={{ color: 'white' }}>Cadastrar</Button>
+
+                    <p>Ao clicar em cadastrar, você declara que leu e concorda com nossos <a href>termos de utilização</a>.</p>
+
+                </FormControl>
             </Stack>
 
-            <Footer/>
+            <Footer />
         </>
     );
 }
